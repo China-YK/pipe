5015edafa3292f0411ce134fe6c8476660494194
小小蛋黄_linux <3037432797@qq.com>
2025-10-20 18:44:13 +0800

    pipe回收机制

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ void singlepipe(){
     int fds[2] = {-1,-1};
     char str[512]="hello";
     char buffer[512];
-    pipe(fds);
+    pipe(fds);//讲一下这个管道的生命周期
+    //当所有与这个管道相关进程都关闭了管道文件描述符后，管道就会被内核自动销毁。
 
     pid_t pid = fork();
     // pipe(fds);
@@ -17,6 +18,8 @@ void singlepipe(){
         // // write(fds[1],str,sizeof(str));//这里如果使用sizeof，则会发送512个字节过去，至于后面那些字节是什么内容无法保证！
         // printf("len:%d\n",len);
         printf("%d %d\n",fds[0],fds[1]);
+        close(fds[1]);//对应父进程来讲，它不用写端之后就可以关闭了
+        close(fds[0]);//不用读端，就直接关闭，此时pipe的引用计数-1，当pipe的引用计数==0时，内核会回收掉pipe
     }else if (pid > 0){
         int len = read(fds[0],buffer,sizeof(buffer));//第三个参数表示最大可以接收多少字节
         printf("len_read:%d\n",len);
@@ -26,9 +29,19 @@ void singlepipe(){
             // printf("hahahha\n");
             int len = read(fds[0],buffer,sizeof(buffer));//水(数据)已经被它的父进程接了(read)，管道里面是空的，同时write的进程也关闭了，所以这里直接返回0，读了空数据
             printf("%s\n",buffer);
+
+            //关闭孙进程的读写端
+            close(fds[0]);
+            close(fds[1]);
         }
         // printf("%d %d\n",fds[0],fds[1]);
+        close(fds[0]);//对应子进程来讲，它不用读端之后就可以关闭了
+        close(fds[1]);//关闭子进程的写端
+        //pipe引用计数减1
     }else exit(-1);
+
+    //此时所有和这个pipe相关的进程，都关闭了他们的读写端，引用计数==0了，pipe被内核回收
+
 }
 // void doublepipe(){
 //     // 创建双管道，实现TCP的应答机制
